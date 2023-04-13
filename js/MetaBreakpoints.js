d0334b7d4033864e3ee8bd26e21b92e79915e186
HHH-GH <hayden@opie-etc.com>
2023-04-13 12:48:24 +0800

    Add the breakpoints and font sizes files

---
+++ js/MetaBreakpoints.js
@@ -0,0 +1,14 @@
+/**
+ * Copy in the breakpoints part from https://github.com/MetaFam/TheGame/blob/develop/packages/design-system/src/theme/index.ts 
+ */
+
+export const metaBreakpoints = {
+	base: '0em',
+	sm: '30em',
+	md: '48em',
+	lg: '62em',
+	xl: '80em',
+	'2xl': '96em',
+	'3xl': '120em',
+	'4xl': '160em',
+}
---
+++ js/MetaFontSizes.js
@@ -0,0 +1,19 @@
+/**
+ * Copy in the fonts part from https://github.com/MetaFam/TheGame/blob/develop/packages/design-system/src/theme/index.ts 
+ */
+
+export const metaFontSizes = {
+	xs: '0.694rem',
+	sm: '0.833rem',
+	md: '1rem',
+	lg: '1.2rem',
+	xl: '1.25rem',
+	'2xl': '1.44rem',
+	'3xl': '1.728rem',
+	'4xl': '2.074rem',
+	'5xl': '2.488rem',
+	'6xl': '2.986rem',
+	'7xl': '3.583rem',
+	'8xl': '4.3rem',
+	'9xl': '5.16rem',
+}
